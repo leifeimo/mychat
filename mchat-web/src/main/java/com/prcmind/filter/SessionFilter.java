7c7e9ccfe98b41baf5d73c06fcb527385d482ebd
leichang <leichang@guozhenlife.cn>
2017-05-12 14:15:44 +0800

    update

--- mchat-web/src/main/java/com/prcmind/filter/SessionFilter.java
+++ mchat-web/src/main/java/com/prcmind/filter/SessionFilter.java
@@ -8,6 +8,12 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.prcmind.utils.WebConstants;
+import com.prcmind.view.BaseUserView;
 
 public class SessionFilter implements Filter {  
     
@@ -19,28 +25,33 @@ public class SessionFilter implements Filter {
     @Override  
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,  
             ServletException {  
-//        HttpServletRequest httpServletRequest = (HttpServletRequest) request;  
-//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;  
-//        StringBuffer url=httpServletRequest.getRequestURL();
-//        if(url.toString().contains("oauth/token")){//过滤登录接口
-//        	chain.doFilter(request, response);  
-//        	return;
-//        }
-//        //过滤get请求、post请求session超时
-//       if( httpServletRequest.getMethod().toLowerCase().equals("get")||httpServletRequest.getMethod().toLowerCase().equals("post")){
-//    	   HttpSession session= httpServletRequest.getSession();
-//           BaseUserView user = (BaseUserView) session.getAttribute(WebConstants.CURRENT_USER);
-//           if(user == null){
-//        	   if (httpServletRequest.getHeader("x-requested-with") != null  
-//                       && httpServletRequest.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {  
-//            	   httpServletResponse.setHeader("sessionstatus", "timeout");//在响应头设置session状态  
-//               } 
-//        	   httpServletResponse.setContentType("text/html; charset=utf-8");
-//        	   PrintWriter writer=  httpServletResponse.getWriter();
-//        	   writer.write("<script>alert('您太久离开页面，请重新登录');</script>");
-//               return;  
-//           }
-//       }
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;  
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;  
+        StringBuffer url=httpServletRequest.getRequestURL();
+        String path = httpServletRequest.getRequestURI();
+        if(url.toString().contains("oauth/token")){//过滤登录接口
+        	chain.doFilter(request, response);  
+        	return;
+        }
+        //过滤get请求、post请求session超时
+       if( httpServletRequest.getMethod().toLowerCase().equals("get")||httpServletRequest.getMethod().toLowerCase().equals("post")){
+    	   HttpSession session= httpServletRequest.getSession();
+           BaseUserView user = (BaseUserView) session.getAttribute(WebConstants.CURRENT_USER);
+           if(user == null){
+        	   if (httpServletRequest.getHeader("x-requested-with") != null  
+                       && httpServletRequest.getHeader("x-requested-with").equalsIgnoreCase("XMLHttpRequest")) {  
+            	   httpServletResponse.setHeader("sessionstatus", "timeout");//在响应头设置session状态  
+               }else{
+            	   if(url.toString().contains("medic")){//过滤登录接口
+            		   httpServletResponse.sendRedirect("/html/login.html");
+                   		return;
+                   }else if(url.toString().contains("enterprise")){
+                	   httpServletResponse.sendRedirect("/html/login.html");
+                   }
+               }
+        	  
+           }
+       }
         chain.doFilter(request, response);  
     }  
   
