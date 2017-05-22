5a936877bac5b6e60ac7f46d1e40cb6324a3932e
leichang <leichang@guozhenlife.cn>
2017-05-22 15:08:18 +0800

    update

--- mchat-api/src/main/java/com/prcmind/net/RestUtil.java
+++ mchat-api/src/main/java/com/prcmind/net/RestUtil.java
@@ -1,17 +1,13 @@
 package com.prcmind.net;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
-
-import com.alibaba.fastjson.JSON;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import com.alibaba.fastjson.JSON;
 public class RestUtil {
     public static final MediaType T_JSON = MediaType.parse("application/json; charset=utf-8");
 
@@ -31,12 +27,4 @@ public class RestUtil {
         Response response = client.newCall(request).execute();
         return response.body().string();
     }
-    public static void main(String[] args) throws IOException {
-		HashMap<String, String> param = new HashMap<String, String>();
-		param.put("scoreNo", "002");
-		param.put("access_token", "01c47875-d39e-421e-b2b2-b0e65cb53f13");
-		String json = JSON.toJSONString(param);
-		String result = RestUtil.postJSON("https://api.prcmind.cn:1600/enterpriseMchat/getMchatQuestionnaireResponse", json);
-		System.out.println(result);
-	}
 }
